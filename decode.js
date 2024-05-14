5e4011650c91cc34bfa4738ba5cb4aba1d4fc363
RendiNurfauzi <85463301+RendiNurfauzi@users.noreply.github.com>
2024-05-14 17:01:02 +0700

    Update decode.js

--- decode.js
+++ decode.js
@@ -1,43 +1,37 @@
 document.getElementById('uploadButton').addEventListener('click', function() {
-    document.getElementById('fileInput').click(); // Memicu klik pada input file
+    document.getElementById('fileInput').click();
 });
 
 document.getElementById('fileInput').addEventListener('change', function(event) {
     const file = event.target.files[0];
     if (file) {
-        // Membuat FileReader untuk membaca file
         const reader = new FileReader();
         
         reader.onload = function(e) {
-            // Mengatur sumber gambar ke hasil pembacaan file
             const imgElement = document.getElementById('previewImage');
             imgElement.src = e.target.result;
             
-            // Memastikan gambar dimuat sebelum menggambar ke canvas
             imgElement.onload = function() {
-                // Mendapatkan canvas dan konteksnya
+
                 const canvas = document.getElementById('hiddenCanvas');
                 const ctx = canvas.getContext('2d');
                 
-                // Menyesuaikan ukuran canvas dengan gambar
+
                 canvas.width = imgElement.naturalWidth;
                 canvas.height = imgElement.naturalHeight;
                 
-                // Menggambar gambar ke canvas
+
                 ctx.drawImage(imgElement, 0, 0, imgElement.naturalWidth, imgElement.naturalHeight);
             };
         };
         
-        // Membaca file sebagai URL data
+
         reader.readAsDataURL(file);
     }
 });
 
 document.getElementById('decodeButton').addEventListener('click', decodeMessageFromImage);
 
-//function decodespreadSpectrum() {
-    
-//}
 
 // Fungsi untuk mengacak array dengan seed
 function shuffleArray(array, seed) {
@@ -75,7 +69,7 @@ function decodeMessageFromImage() {
     let bits = [];
     for (let i = 0; i < data.length; i += 8) {
         const pixelIndex = pixelIndices[i];
-        const bit = data[pixelIndex] & 1; // Mengambil least significant bit
+        const bit = data[pixelIndex] & 1; 
         bits.push(bit);
     }
 
@@ -91,7 +85,7 @@ function bitArrayToString(bitArray) {
             byte |= bitArray[i + j] << (7 - j);
         }
         if (byte === 0) {
-            break; // Menghentikan pembacaan jika menemukan byte 0, asumsi sebagai terminator
+            break; 
         }
         string += String.fromCharCode(byte);
     }
